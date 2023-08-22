6f5c3a2f4564a004ee2c437c6b62a7d574e3c897
Jim Mack <jim@mack.bz>
2023-08-21 23:32:24 -0700

    boo boo

--- argocd-install-cm.sh
+++
@@ -1,9 +0,0 @@
-argocd app create cert-manager-config \
-  --repo https://github.com/jtmack6/helm-chart \
-  --helm-chart cert-manager-install \
-  --revision 0.1.0 \
-  --dest-namespace default \
-  --dest-server 'https://kubernetes.default.svc' \
-  --helm-set acme_email=nordac@gmail.com \
-  --helm-set cloudflare_api_token=VnMOJdBbGHg0wyiZkqV1G-BOquOhfHa7hRqVXKwW \
-  --helm-set cert-manager.enabled=true
